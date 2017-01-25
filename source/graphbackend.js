57f758689dd99038a381a7fc37dbf67166e8505c
Shemamforash <samwest2@hotmail.co.uk>
2017-01-25 16:18:42 +0000

    Tag Editor Backend (In Progress)

--- source/graphbackend.js
+++ source/graphbackend.js
@@ -1,30 +1,29 @@
 /**
  * Created by Sam on 20/01/2017.
  */
-var graph = (function(){
+var graph = (function () {
     var nodes = {};
     var edges = [];
-    var tags = [];
     var i;
 
-    function node_overlaps(x, y, id){
-        for(var node in nodes){
+    function node_overlaps(x, y, id) {
+        for (var node in nodes) {
             node = nodes[node];
-            if(node.x === x && node.y === y && id !== node.id) {
+            if (node.x === x && node.y === y && id !== node.id) {
                 return true;
             }
         }
         return false;
     }
 
-    function create_edge(origin, end){
+    function create_edge(origin, end) {
         return {
             origin: origin,
             end: end
         };
     }
 
-    function create_node(id, x, y){
+    function create_node(id, x, y) {
         return {
             id: id,
             x: x,
@@ -33,26 +32,26 @@ var graph = (function(){
     }
 
     return {
-        add_edge: function(origin, end) {
-            for(i = 0; i < edges.length; ++i) {
+        add_edge: function (origin, end) {
+            for (i = 0; i < edges.length; ++i) {
                 var e = edges[i];
-                if((e.origin === origin && e.end === end) || (e.end === origin && e.origin === end)){
+                if ((e.origin === origin && e.end === end) || (e.end === origin && e.origin === end)) {
                     return false;
                 }
             }
             edges.push(create_edge(origin, end));
             return true;
         },
-        add_node: function(id, x, y) {
-            if(node_overlaps(x, y, id)){
+        add_node: function (id, x, y) {
+            if (node_overlaps(x, y, id)) {
                 return false;
             }
             nodes[id] = create_node(id, x, y);
             return true;
         },
-        update_node_position: function(node){
+        update_node_position: function (node) {
             var graph_node = nodes[node.id()];
-            if(node_overlaps(node.position().x, node.position().y, node.id())){
+            if (node_overlaps(node.position().x, node.position().y, node.id())) {
                 node.position().x = graph_node.x;
                 node.position().y = graph_node.y;
             } else {
@@ -60,11 +59,72 @@ var graph = (function(){
                 graph_node.y = node.position().y;
             }
         },
-        update_node_property: function(node, prop, val) {
+        update_node_property: function (node, prop, val) {
             var graph_node = nodes[node.id()];
-            if(graph_node.hasOwnProperty(prop)){
+            if (graph_node.hasOwnProperty(prop)) {
                 graph_node[prop] = val;
             }
+        }
+    }
+}());
+
+//Manages the current list of tags in the system
+//Tags must be unique and can only be declared in the graph sidebar
+var tags = (function () {
+    var tag_list = [];
+    var i;
+
+    function tag_exists(tag_name, element) {
+        for (i = 0; i < tag_list.length; ++i) {
+            if (tag_list[i].tag_name === tag_name) {
+                if(element){
+                    if(tag_list[i].element === element){
+                        return tag_list[i];
+                    }
+                    return null;
+                }
+                return tag_list[i];
+            }
+        }
+        return null;
+    }
+
+    return {
+        //Returns false if a tag with the given name already exists
+        //Tags are pairs of a name and an element correspondign to the element in the graph sidebar
+        add_tag: function (element, tag_name) {
+            if (tag_exists(tag_name)) {
+                return false;
+            }
+            tag_list.push({
+                element: element,
+                tag_name: tag_name
+            });
+            console.log(element);
+            return true;
+        },
+        //Returns false if no tag found
+        //Otherwise removes the tag and returns true
+        remove_tag: function (element, tag_name) {
+            var tag = tag_exists(tag_name);
+            if (tag) {
+                tag_list.remove(tag);
+                return true;
+            }
+            return false;
+        },
+        //Takes the old name of a tag and replaces it with a new one
+        //Returns true if the tag exists, otherwise false
+        update_tag: function (old_name, new_name) {
+            var tag = tag_exists(old_name);
+            if (tag) {
+                tag.tag_name = new_name;
+                return true;
+            }
+            return false;
+        },
+        is_tag_bound: function(tag_name, element){
+            return tag_exists(tag_name, element);
         }
     }
 }());
--- source/htmlbehaviour.js
+++ source/htmlbehaviour.js
@@ -1,26 +1,22 @@
 /**
  * Created by Sam on 22/01/2017.
  */
-var sidebarOpen = false;
+var sidebarOpen = 'hide';
 var editing = false;
 
 function toggleGraphSidebar() {
-    if (sidebarOpen) {
-        $('#graph_sidebar').width("10px");
-        $('#toggle_sidebar').text("");
+    if ($('graph_sidebar').hasClass('visible')) {
+        $('#graph_sidebar').sidebar('hide');
     } else {
-        $('#graph_sidebar').width("20%");
-        $('#toggle_sidebar').text("<");
+        $('#graph_sidebar').sidebar('show');
     }
-    // setTimeout(function(){cy.resize(), cy.layout()}, 500);
-    sidebarOpen = !sidebarOpen;
 }
 
 function toggleNodeSidebar(open) {
     if (open) {
-        $('#node_sidebar').width("20%");
+        $('#node_sidebar').sidebar('show');
     } else {
-        $('#node_sidebar').width("0%");
+        $('#node_sidebar').sidebar('hide');
     }
 }
 
@@ -28,20 +24,43 @@ function changeNodeLabel(text) {
     currentNode.get().data('label', text);
 }
 
-function setEditingMode(mode){
+function setEditingMode(mode) {
     editing = mode;
     if (mode) {
+        //User is editing tag names
         $('.tag_edit').removeClass('disabled');
         var delete_button = $('<div class="ui icon button tag_delete inverted right attached"><i class="remove icon"></i></div>');
-        delete_button.click(function(){
+        delete_button.click(function () {
             $(this).parent().remove();
         });
         $('.tag_edit').append(delete_button);
         $('.tag_edit > .tag.icon').remove();
     } else {
+        //User has finished editing tags
         $('.tag_edit').addClass("disabled");
         $('.tag_edit > .tag_delete').remove();
-        $('.tag_edit').append($('<i class="tag icon inverted"></i>"'));
+        var tag_entries = $('.tag_edit')
+        for (var i = 0; i < tag_entries.length; ++i) {
+            if ($(tag_entries[0]).find('.tag.icon').length === 0) {
+                $('.tag_edit').append($('<i class="tag icon"></i>"'));
+            }
+        }
+    }
+}
+
+function checkNewTags() {
+    var tag_elements = $('.tag_edit > input');
+    for (var i = 0; i < tag_elements.length; ++i) {
+        var tag_name = $(tag_elements[i]).val();
+        if (tag_name !== "") {
+            if (!tags.is_tag_bound(tag_name, tag_elements[i])) {
+                if (!tags.add_tag($(tag_elements[i]), tag_name)) {
+                    $(tag_elements[i]).val("");
+                }
+            } else {
+                console.log("bound");
+            }
+        }
     }
 }
 
@@ -49,7 +68,8 @@ $(document).ready(function () {
     $('.ui.accordion').accordion({
         exclusive: false
     });
-    $('#graph_sidebar').sidebar('attach events', '#sidebar_opener');
+    $('.ui.dropdown').dropdown();
+    // $('#graph_sidebar').sidebar('attach events', '#sidebar_opener');
     $('.ui.sidebar').sidebar({
         transition: 'overlay',
         dimPage: false,
@@ -65,13 +85,28 @@ $(document).ready(function () {
     $('#add_tag_button').click(function () {
         setEditingMode(false);
         $('#tag_editor').before($('<div class="ui inverted transparent fluid">' +
-                                    '<div class="ui icon disabled input inverted transparent tag_edit">' +
-                                        '<input type="text" placeholder="Tag name...">' +
-                                        '<i class="tag icon inverted"></i>' +
-                                    '</div>' +
-                                '</div>'));
+            '<div class="ui icon disabled input inverted transparent tag_edit">' +
+            '<input type="text" placeholder="Tag name...">' +
+            '<i class="tag icon"></i>' +
+            '</div>' +
+            '</div>'));
     });
     $('#edit_tags_button').click(function () {
         setEditingMode(!editing);
+        var icon = $('#edit_tags_button > .icon');
+        if ($('#edit_tags_button').text().indexOf("Edit") !== -1) {
+            $('#edit_tags_button').html("Done");
+        } else {
+            $('#edit_tags_button').html("Edit");
+            checkNewTags();
+        }
+        $('#edit_tags_button').prepend(icon);
+        $('#edit_tags_button > .icon').toggleClass('checkmark');
+        $('#edit_tags_button > .icon').toggleClass('edit');
+    });
+    $('#edit_node_name').click(function () {
+        $('#edit_node_name > .icon').toggleClass('edit');
+        $('#edit_node_name > .icon').toggleClass('checkmark');
+        $('#edit_node_name').parent().toggleClass('disabled');
     });
 });
