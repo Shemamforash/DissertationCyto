8a6902e3082a194b970223b28446261fb489fe20
Shemamforash <samwest2@hotmail.co.uk>
2017-01-25 10:47:38 +0000

    Sorting out the Tag Editor

--- source/htmlbehaviour.js
+++ source/htmlbehaviour.js
@@ -2,6 +2,7 @@
  * Created by Sam on 22/01/2017.
  */
 var sidebarOpen = false;
+var editing = false;
 
 function toggleGraphSidebar() {
     if (sidebarOpen) {
@@ -13,22 +14,6 @@ function toggleGraphSidebar() {
     }
     // setTimeout(function(){cy.resize(), cy.layout()}, 500);
     sidebarOpen = !sidebarOpen;
-}
-
-function expandGraphSideBar() {
-    if (!sidebarOpen) {
-        // $('#graph_sidebar').width("40px");
-        // $('#sidebar_opener').text(">");
-        // $('#sidebar_opener').width("40px");
-    }
-}
-
-function minimiseGraphSideBar() {
-    if (!sidebarOpen) {
-        // $('#graph_sidebar').width("10px");
-        // $('#sidebar_opener').text("");
-        // $('#sidebar_opener').width("10px");
-    }
 }
 
 function toggleNodeSidebar(open) {
@@ -43,23 +28,48 @@ function changeNodeLabel(text) {
     currentNode.get().data('label', text);
 }
 
+function setEditingMode(mode){
+    editing = mode;
+    if (mode) {
+        $('.tag_edit').removeClass('disabled');
+        var delete_button = $('<div class="ui icon button tag_delete inverted"><i class="remove icon"></i></div>');
+        delete_button.click(function(){
+            $(this).parent().remove();
+        });
+        $('.tag_edit').append(delete_button);
+    } else {
+        $('.tag_edit').addClass("disabled");
+        $('.tag_edit > .tag_delete').remove();
+    }
+}
+
 $(document).ready(function () {
     $('.ui.accordion').accordion({
         exclusive: false
     });
-    $('.ui.sidebar').sidebar('toggle');
     $('#graph_sidebar').sidebar('attach events', '#sidebar_opener');
-    // $('.sidebar').sidebar('setting', {
-    //     dimPage: false,
-    //     defaultTransition: {
-    //         computer : {
-    //             left: 'overlay'
-    //         }
-    //     }
-    // });
     $('.ui.sidebar').sidebar({
         transition: 'overlay',
         dimPage: false,
         exclusive: true
+    });
+    $('#sidebar_opener').hover(function () {
+        $(this).width("40px");
+        $(this).text(">");
+    }, function () {
+        $(this).width("15px");
+        $(this).text("");
+    });
+    $('#add_tag_button').click(function () {
+        setEditingMode(false);
+        $('#tag_editor').before($('<div class="ui inverted segment tag_edit">' +
+                                    '<div class="ui inverted transparent fluid icon input">' +
+                                        '<input type="text" placeholder="tag">' +
+                                        '<i class="tag icon inverted"></i>' +
+                                    '</div>' +
+                                '</div><div class="ui divider inverted"></div>'));
+    });
+    $('#edit_tags_button').click(function () {
+        setEditingMode(!editing);
     });
 });
